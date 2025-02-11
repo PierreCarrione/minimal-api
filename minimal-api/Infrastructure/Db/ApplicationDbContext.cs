3f68bba53bbd0fd02f53c13be6c235e7df07d1e2
PierreCarrione <pierre.rossi@hotmail.com>
2025-02-11 11:42:37 -0300

    Add the "Administrator Entity", "ApplicationDbContext" and  the "Administrator migration".

--- minimal-api/Infrastructure/Db/ApplicationDbContext.cs
+++ minimal-api/Infrastructure/Db/ApplicationDbContext.cs
@@ -1,8 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using minimal_api.Domain.Entities;
 
 namespace minimal_api.Infrastructure.Db
 {
     public class ApplicationDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Administrator>().HasData(
+                new Administrator
+                {
+                    Id = 1,
+                    Email = "administrador@teste.com",
+                    Password = "123456",
+                    Profile = "Adm"
+                }    
+            );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            var conectionString = _configuration.GetConnectionString("MySql")?.ToString();
+
+            if (!string.IsNullOrEmpty(conectionString))
+            {
+                optionsBuilder.UseMySql(conectionString, ServerVersion.AutoDetect(conectionString));
+            }
+        }
+
+        public DbSet<Administrator> Administrators { get; set; }
     }
 }
--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using minimal_api.Domain.DTOs;
+using minimal_api.Infrastructure.Db;
+
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//{
+//    options.UseMySql(
+//        builder.Configuration.GetConnectionString("MySql"),
+//        ServerVersion.AutoDetect("MySql")
+//    );
+//});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Password == "123456")
+        return Results.Ok("Login com sucesso");
+    else
+        return Results.Unauthorized();
+});
 
 app.Run();
